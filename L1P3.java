17f1c288c2413f843a4ab0faa17f73810c26f0f4
SixtoCalvario <sixtytwocalvario@gmail.com>
2024-02-13 13:48:40 -0500

    y

--- L1P3.java
+++ L1P3.java
@@ -1,5 +1,18 @@
 // Write a program that asks a user to enter 9 numbers.  Find and display the total of those numbers.  Use a while loop.
 import java.util.Scanner;
 public class L1P3 {
-    
+    public static void main(String[] args) {
+        // make scanner object
+        canner input = new Scanner(System.in);
+
+        // take 9 inputs and display sum
+        int inputs = 0;
+        int sum = 0;
+        while(inputs!=9) {
+            System.out.print("Enter 9 numbers: ");
+            int inputs = input.nextInt();
+            sum+=inputs;
+        }
+        System.out.print("Your sum is: "+sum);
+    }
 }
