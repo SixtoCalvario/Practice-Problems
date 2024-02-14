e3d596c50e24e976d4017fb5273ef458556357d0
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-02-14 23:02:13 +0000

    DONE RAHHH wait no nvm

--- L2P3.java
+++ L2P3.java
@@ -6,8 +6,28 @@ Be sure not to include the -999 in the total.
 At the end of the program, output the total of the number.  
 Use a while loop.
 */
+import java.util.Scanner;
 public class L2P3 {
     public static void main(String[] args) {
+        // scanner
+        Scanner input = new Scanner(System.in);
         
+        // control variable
+        int num = 0;
+        int sum = 0;
+        // print statement
+        System.out.println("Enter numbers to find the sum when you are done enter -999");
+
+        // condition
+        while (num!=-999) {
+            // body
+            System.out.print("Enter your num here: ");
+            num = input.nextInt();
+            if (num!=-999) {
+                sum+=num;
+            }
+        }
+        System.out.println("your sum is: "+sum);
+        input.close();
     }
 }
--- L2P4.java
+++ L2P4.java
@@ -5,6 +5,42 @@
  At the end of the program, if the total is above 21, display the word “BUST.” 
  If the total is exactly 21, display the word “WINNER.” Otherwise, display the total score in the form “Score: <total>.”  
  */
+import java.util.Scanner;
 public class L2P4 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // generate 2 random nums
+        int num1 = (int)(Math.random()*11+1);
+        int num2 = (int)(Math.random()*11+1);
+        
+        // find and display sum
+        int sum = num1+num2;
+        System.out.println("The total of the two nums is: "+sum);
+
+        // control variable
+        String hit = "HIT";
+
+        // while
+        while (hit.equals("HIT")) {
+            System.out.print("Enter: ");
+            hit = input.nextLine();
+            int num3 = (int)(Math.random()*11+1);
+            System.out.println("ANOTHER NUM BETWEEN 1-11 HAS BEEN ADDED");
+            sum+=num3;
+        }
+
+        // if sum is over 21
+        if (sum>21) {
+            System.out.println("BUST");
+        }
+        // if sum is 21
+        if (sum==21) {
+            System.out.println("WINNER");
+        }
+        // if sum is under 21
+        if (sum<21) {
+            System.out.println("Score: "+sum);
+        }
+    }
 }
