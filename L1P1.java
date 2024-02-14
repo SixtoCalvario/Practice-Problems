c2f1ad6b6452dbd7bf3065a98c47698b8e8b409d
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-02-14 23:06:39 +0000

    NOW IM DONE

--- L1P1.java
+++ L1P1.java
@@ -1,3 +1,18 @@
+//Write a program that asks the user to enter a number between 10 and 20.  Then, print “Java” on the screen that many times.  Use a while loop. 
+import java.util.Scanner;
 public class L1P1 {
-    
+    public static void main(String[] args) {
+        // SCANNER
+        Scanner input = new Scanner(System.in);
+        // taking input
+        System.out.print("Enter a num between 10-20: ");
+        int num = input.nextInt();
+        input.close();
+        // control var
+        int runs = 0;
+        while (runs!=num) {
+            System.out.println("JAVA");
+            runs++;
+        }
+    }
 }
