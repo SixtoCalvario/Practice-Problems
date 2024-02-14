2ae7847f89322c429e31cc654e76449a039845ce
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-02-14 15:46:20 -0500

    olsbdnvg

---
+++ L1P4.java
@@ -0,0 +1,8 @@
+// NO WAYT IUT DIDNT SAVE I FORGOT TO SAVE IT U HJSUT TURNED OF MY PC IM SO MADMIEKJGB!>!!!!!!!!!!!!
+// anyways Ask a user if they want to continue.  The program should keep running for any input they type except no.  Once the user enters no, exit the program. Use a while loop.
+import java.util.Scanner;
+public class L1P4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
