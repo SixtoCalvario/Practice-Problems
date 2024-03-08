24c8b634040ef87adc90dea08eeae3ac31b5ef4e
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-03-08 04:12:24 +0000

    ji

---
+++ Arrays/L2P3.java
@@ -0,0 +1,16 @@
+/*
+ Create a character array of size 35. 
+ Allow the user to enter letters into the array as long as the letters are uppercase and 
+ they don’t exceed the available storage space of the array. 
+ As soon as they enter a lowercase letter, the loop should terminate and 
+ the program should output the physical and logical sizes of the array.
+ 
+ */
+package Arrays;
+import java.util.Scanner;
+public class L2P3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
