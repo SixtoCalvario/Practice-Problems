6cb73b2d1e1c1c8654c50c127cbedf8b08a13623
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-03-08 18:25:05 +0000

    msg

--- Arrays/L2P5.java
+++ Arrays/L2P5.java
@@ -3,6 +3,19 @@ The Java String class has a method called split that takes a string parameter.
 This parameter is called a delimiter and is used to separate items of data. 
 So, if a user enters a set of numbers in one line, separated by commas: 
 1,3,5,2,4,6 and that data is stored in the string, input, the information can be separated into an array as follows:
- String[ ] arr = input.split(“,”);. Using this idea, take a list of integers from a user (of any length) separated by commas, store them in a String array, convert that array to an int array, and then find the average of the numbers.
+String[ ] arr = input.split(“,”);. Using this idea, take a list of integers from a user (of any length)
+separated by commas, store them in a String array, convert that array to an int array, 
+and then find the average of the numbers.
 
 */
+package Arrays;
+import java.util.Scanner;
+public class L2P5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a list of numbers seperated by commas: ");
+        String str = input.nextLine();
+        String[] strArray = strArray.split(",")
+
+    }
+}
---
+++ Arrays/L2P6.java
@@ -0,0 +1,17 @@
+/*
+ Create a program that fills an array with fifteen random numbers between 1 and 100. 
+ Use a loop to search the list for the smallest number. 
+ Display the list, the smallest number and its location.
+ */
+package Arrays;
+
+/**
+ * L2P6
+ */
+public class L2P6 {
+
+    public static void main(String[] args) {
+        int intArray = new Int[15];
+        
+    }
+}
