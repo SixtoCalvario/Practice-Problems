1fc931771b95c44c933b23d8112ea6068e0f5378
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-03-14 12:25:35 +0000

    msg

--- Arrays/L3P1.java
+++ Arrays/L3P1.java
@@ -6,8 +6,19 @@
     Also, print the list so that the user can verify your results. 
 */
 package Arrays;
+import java.util.Scanner; 
 public class L3P1 {
     public static void main(String[] args) {
-        
+        // scanner :D
+        Scanner input = new Scanner(System.in);
+        int[] intArray = new int[20];
+        // for loop to assign 20 random nums to the array 
+        for(int i=0; i<20; i++) {
+            intArray[i] = (int)(Math.random()*30+1);
+        }
+        System.out.print("What number do you want to search for? : ");
+        int userInt = input.nextInt();
+        input.close();
+        // finding num if not it outputs -1
     }
 }
